d272f145448718b1f003ec771ab67f80e1800ab7
Caitlin Tran <Caitlin.Tran@ibm.com>
2025-06-04 16:12:02 -0400

    Setting up files

---
+++ src/business_case_assistant/graph.py
@@ -0,0 +1,65 @@
+from langchain_aws import ChatBedrock
+from langgraph.graph import StateGraph, END, MessagesState
+import os
+
+from agent.nodes import(BusinessCaseState, 
+    gather_requirements, 
+    write_alternatives_and_analysis, 
+    write_introduction, 
+    write_executive_summary, 
+    write_general_project_information, 
+    write_high_level_business_impact, 
+    write_preferred_solution, 
+    compile_document, 
+    route_gather)
+
+profile = os.getenv("AWS_PROFILE", None)
+
+medium_model = ChatBedrock(
+    model_id = "us.meta.llama3-2-90b-instruct-v1:0",
+    credentials_profile_name = profile,
+    max_tokens = 2000,
+    temperature = 0,
+    )
+
+small_model = ChatBedrock(
+    model_id = "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    credentials_profile_name = profile,
+    max_tokens = 4000,
+    temperature = 0,
+    )
+
+def build_graph():
+    # Create the graph
+    graph = StateGraph(BusinessCaseState, input= MessagesState)
+
+    # Add nodes to the graph
+    graph.add_node("Gathering Requirements", lambda state: gather_requirements(state, small_model))
+    graph.add_node("Writing Introduction",lambda state: write_introduction(state, small_model))
+    graph.add_node("Writing General Project Information", lambda state: write_general_project_information(state, medium_model))
+    graph.add_node("Writing High Level Business Impact", lambda state: write_high_level_business_impact(state, medium_model))
+    graph.add_node("Writing Alternatives and Analysis", lambda state: write_alternatives_and_analysis(state, small_model, medium_model))
+    graph.add_node("Writing Preferred Solution", lambda state: write_preferred_solution(state, medium_model))
+    graph.add_node("Writing Executive Summary",lambda state: write_executive_summary(state, small_model))
+    graph.add_node("Compiling Document", compile_document)
+
+    # Define the edges
+
+    graph.set_entry_point("Gathering Requirements")
+    graph.add_conditional_edges("Gathering Requirements", route_gather)
+    graph.add_edge("Writing Introduction", "Writing General Project Information")
+    graph.add_edge("Writing Introduction", "Writing High Level Business Impact")
+    graph.add_edge("Writing Introduction", "Writing Alternatives and Analysis")
+    graph.add_edge("Writing Introduction", "Writing Preferred Solution")
+    graph.add_edge(
+        ["Writing General Project Information", 
+        "Writing High Level Business Impact", 
+        "Writing Alternatives and Analysis", 
+        "Writing Preferred Solution"], 
+        "Writing Executive Summary")
+    graph.add_edge("Writing Executive Summary", "Compiling Document")
+    graph.add_edge("Compiling Document", END)
+
+    return graph
+
+graph = build_graph().compile()
